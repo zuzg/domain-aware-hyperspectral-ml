7b79a7762cf29b76c36792583d006368034821db
Zuza Gawrysiak <gawrysiak.zuzanna@gmail.com>
2025-08-27 19:19:57 +0200

    Change model_config to config

--- src/options.py
+++ src/options.py
@@ -8,21 +8,21 @@ from src.config import ExperimentConfig
 
 @dataclass
 class Args:
-    model_config: str
+    config: str
 
     @classmethod
     def from_cli(cls) -> "Args":
         parser = argparse.ArgumentParser()
-        parser.add_argument("--model_config", type=str, default="configs/dummy.yaml")
+        parser.add_argument("--config", type=str, default="configs/dummy.yaml")
         args = parser.parse_args()
         return Args(
-            model_config=args.model_config,
+            config=args.config,
         )
 
 
 def parse_args() -> ExperimentConfig:
     args = Args.from_cli()
-    with open(args.model_config) as file:
+    with open(args.config) as file:
         config_data = yaml.safe_load(file)
         cfg = ExperimentConfig(**config_data)
 
